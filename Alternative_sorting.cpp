a0cd65a9c1900ba02a50f9d0eae269624dfd596b
PrajwalSinghBhadouria <151111886+PrajwalSinghBhadouria@users.noreply.github.com>
2024-10-25 22:07:01 +0530

    Add files via upload

---
+++ Alternative_sorting.cpp
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+
+  public:
+    vector<int> alternateSort(vector<int>& arr) {
+        // Your code goes here
+        sort(arr.begin(),arr.end());
+        vector<int> v;
+        int j = arr.size()-1;
+        for(int i=0; i<arr.size()/2; i++){
+            v.push_back(arr[j-i]);
+            v.push_back(arr[i]);
+        }
+        if((j+1) %2 != 0){
+            v.push_back(arr[(j+1)/2]);
+        }
+        return v;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution obj;
+        vector<int> ans = obj.alternateSort(arr);
+        for (int i = 0; i < ans.size(); i++) {
+            cout << ans[i] << " ";
+        }
+        cout << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
